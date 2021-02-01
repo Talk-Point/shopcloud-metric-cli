04a3175df70a68f3d21cee69647cbd5854fa27f9
Konstantin.Stoldt <konstantin.stoldt@talk-point.de>
2021-02-01 13:27:47 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='shopcloud_metric',
-    version='1.0.0',
+    version='0.1.0',
     description='CLI tool for Metrics',
     long_description_content_type="text/markdown",
     long_description=README,
