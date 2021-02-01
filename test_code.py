f6b8710ec740230fff2c8717b40b01925343037f
Konstantin.Stoldt <konstantin.stoldt@talk-point.de>
2021-02-01 13:36:45 +0100

    Bug:Fix secrethub token

--- test_code.py
+++ test_code.py
@@ -1,4 +1,8 @@
+import os
+
 def test_job_metric():
+    os.environ["SECRETHUB_TOKEN"] = "123456"
+    
     from shopcloud_metric import JobMetric
 
     job = JobMetric('sage-supplier-update', 
@@ -7,4 +11,3 @@ def test_job_metric():
     )
     with job as j:
         j.gauge('job_last_success_unixtime', labels={'supplier': 'EP'})
-    
